319bfe9c22e08c3dc28b2a6e6e4081c69111b468
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-12-14 05:59:34 +0900

    프로그래머스[고득점 kit] - 완주하지 못한 선수

---
+++ Programmers/Incomplete_Participant_Sol1/src/Main.java
@@ -0,0 +1,40 @@
+package CodingTest.Programmers.Incomplete_Participant_Sol1.src;
+
+import java.io.IOException;
+import java.util.*;
+
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        String[] participant= {"leo", "kiki", "eden"};
+//        String[] completion = {"eden", "kiki"};
+//
+//        Solution_1 s = new Solution_1();
+//       String result =  s.solution(participant, completion);
+//
+//       System.out.println(result);
+//    }
+//}
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        int flag = 0;
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i = 0; i < participant.length -1; i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0){
+            answer = participant[participant.length-1];
+        }
+
+        return answer;
+    }
+}
---
+++ Programmers/Incomplete_Participant_Sol2/src/Main.java
@@ -0,0 +1,31 @@
+package CodingTest.Programmers.Incomplete_Participant_Sol2.src;
+
+import java.io.IOException;
+import java.util.*;
+
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String p : participant){
+            map.put(p,map.getOrDefault(p,0)+1);
+        }
+        for(String c : completion){
+            map.put(c, map.get(c)-1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+
+            if(entry.getValue() != 0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+        return answer;
+    }
+}
